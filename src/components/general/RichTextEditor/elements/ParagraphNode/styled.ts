788c02f07ef4f90d9bb19dfd99bfa9cb4af3d289
Leroy Korterink <lkorterink@outlook.com>
2019-12-05 23:47:46 +0100

    Remove unused function

--- src/components/general/RichTextEditor/elements/ParagraphNode/styled.ts
+++ src/components/general/RichTextEditor/elements/ParagraphNode/styled.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import Paragraph from "../../../Paragraph";
 
 export const StyledParagraph = styled(Paragraph)`
